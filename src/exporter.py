40caaf7ea50df9068eb315f40a2bf1368acbe25b
Nytelife26 <xtylerjrx@gmail.com>
2024-02-28 02:24:21 +0000

    refactor: tidy up

--- src/exporter.py
+++ src/exporter.py
@@ -1,208 +1,191 @@
-# Import necessary libraries
-import subprocess
-import json
-import os
-import logging
-import datetime
-from prometheus_client import make_wsgi_app, Gauge, Info
-from flask import Flask
-from waitress import serve
-from shutil import which
+"""cfspeedtest exporter for Prometheus."""
 
-# Create a Flask application
+import datetime
+import json
+import logging
+import os
+import subprocess
+import sys
+from collections.abc import Callable
+from shutil import which
+from typing import NamedTuple
+
+from flask import Flask
+from prometheus_client import Gauge, Info, make_wsgi_app
+from waitress import serve
+
 app = Flask("Cloudflare-Speedtest-Exporter")
 
 # Setup logging with a specific format and level
-format_string = "level=%(levelname)s datetime=%(asctime)s %(message)s"
-logging.basicConfig(encoding="utf-8", level=logging.DEBUG, format=format_string)
+logging.basicConfig(
+    encoding="utf-8",
+    level=logging.DEBUG,
+    format="level=%(levelname)s datetime=%(asctime)s %(message)s",
+)
 
 # Disable Waitress Logs
-log = logging.getLogger("waitress")
-log.disabled = True
+logging.getLogger("waitress").disabled = True
 
 # Create Prometheus metrics
-server = Info("speedtest_server", "Speedtest server used to test")
-ping = Gauge(
-    "speedtest_ping_latency_milliseconds", "Speedtest current Ping in ms"
-)
+server = Info("speedtest_server", "Server used for the speedtest")
+ping = Gauge("speedtest_ping_latency_milliseconds", "Speedtest ping in ms")
 jitter = Gauge(
-    "speedtest_jitter_latency_milliseconds", "Speedtest current Jitter in ms"
+    "speedtest_jitter_latency_milliseconds", "Speedtest jitter in ms"
 )
 download_speed = Gauge(
     "speedtest_download_bits_per_second",
-    "Speedtest current Download Speed in bit/s",
+    "Measured download speed in bit/s",
 )
 upload_speed = Gauge(
     "speedtest_upload_bits_per_second",
-    "Speedtest current Upload speed in bits/s",
+    "Measured upload speed in bit/s",
 )
-up = Gauge("speedtest_up", "Speedtest status whether the scrape worked")
+up = Gauge("speedtest_up", "Speedtest status (via scrape)")
 
 # Set cache duration for metrics (in seconds)
-cache_seconds = int(os.environ.get("SPEEDTEST_CACHE_FOR", 0))
+CACHE_SECS = datetime.timedelta(int(os.environ.get("SPEEDTEST_CACHE_FOR", 0)))
 cache_until = datetime.datetime.fromtimestamp(0)
+TEST_CMD = ["cfspeedtest", "--json"]
 
 
-# Define utility functions for unit conversions and JSON validation
-def bytes_to_bits(bytes_per_sec):
-    return bytes_per_sec * 8
+# TODO(?): factor out bps as calculated values
+class TestResult(NamedTuple):
+    """Result of a speedtest."""
+
+    server_city: str
+    server_region: str
+    ping: float
+    jitter: float
+    download_mbps: float
+    download_bps: int
+    upload_mbps: float
+    upload_bps: int
+    status: int
 
 
-def bits_to_megabits(bits_per_sec):
-    megabits = round(bits_per_sec * (10**-6), 2)
-    return str(megabits) + "Mbps"
+# This can be done with defaults, but it seems better to mandate constructing
+# an empty result deliberately
+NULL_RESULT = TestResult("", "", 0, 0, 0, 0, 0, 0, 0)
 
 
-def megabits_to_bits(megabits):
-    bits_per_sec = float(megabits) * (10**6)
-    return str(bits_per_sec)
+def megabits_to_bits(megabits: float) -> int:
+    """
+    Convert Mbit/s to bit/s.
+
+    NOTE: This truncates, because fractional bits would constitute a logic
+    error.
+    """
+    return int(megabits * (10**6))
 
 
-def is_json(myjson):
+def run_test() -> TestResult:
+    """Run the speedtest and parse the results."""
+    timeout = int(os.environ.get("SPEEDTEST_TIMEOUT", "90"))
+
     try:
-        json.loads(myjson)
-    except ValueError:
-        return False
-    return True
-
-
-# Define function to run the speed test and parse the results
-def runTest():
-    timeout = int(os.environ.get("SPEEDTEST_TIMEOUT", 90))
-
-    cmd = ["cfspeedtest", "--json"]
-    # Execute the command and handle potential errors
-    try:
-        output = subprocess.check_output(cmd, timeout=timeout)
-        output = output.decode().rsplit("}", 1)[0] + "}"
-    except subprocess.CalledProcessError as e:
-        output = e.output
-        if not is_json(output):
-            if len(output) > 0:
-                logging.error(
-                    "CFSpeedtest CLI Error occurred that"
-                    + "was not in JSON format"
-                )
-            return (0, 0, 0, 0, 0, 0, 0, 0, 0)
-    except subprocess.TimeoutExpired:
-        logging.error(
-            "CFSpeedtest CLI process took too long to complete "
-            + "and was killed."
+        output = (
+            subprocess.check_output(TEST_CMD, timeout=timeout).decode().strip()
         )
-        return (0, 0, 0, 0, 0, 0, 0, 0, 0)
+    except subprocess.CalledProcessError:
+        logging.exception("cfspeedtest CLI failed.")
+        return NULL_RESULT
+    except subprocess.TimeoutExpired:
+        logging.error("cfspeedtest CLI timed out and was stopped.")
+        return NULL_RESULT
 
-    # Parse the output if it is in JSON format
-    if is_json(output):
+    try:
         data = json.loads(output)
-        if "error" in data:
-            # Socket error
-            print("Something went wrong")
-            print(data["error"])
-            return (0, 0, 0, 0, 0, 0, 0, 0, 0)  # Return all data as 0
-        if "version" in data:
-            actual_server_city = data["test_location_city"]["value"]
-            actual_server_region = data["test_location_region"]["value"]
-            actual_ping = data["latency_ms"]["value"]
-            actual_jitter = data["Jitter_ms"]["value"]
-            download_mbps = data["90th_percentile_download_speed"]["value"]
-            download = megabits_to_bits(download_mbps)
-            upload_mbps = data["90th_percentile_upload_speed"]["value"]
-            upload = megabits_to_bits(upload_mbps)
-            return (
-                actual_server_city,
-                actual_server_region,
-                actual_ping,
-                actual_jitter,
-                download_mbps,
-                download,
-                upload_mbps,
-                upload,
-                1,
-            )
+    except ValueError:
+        logging.error(
+            "cfspeedtest CLI did not produce valid JSON. Received:\n%s", output
+        )
+        return NULL_RESULT
+
+    if "error" in data:
+        # Socket error
+        logging.error("Something went wrong.\nError: %s", data["error"])
+        return NULL_RESULT
+    # TODO: is there a better way to test if data is valid?
+    if "version" not in data:
+        return NULL_RESULT
+
+    return TestResult(
+        data["test_location_city"]["value"],
+        data["test_location_region"]["value"],
+        data["latency_ms"]["value"],
+        data["Jitter_ms"]["value"],
+        download_mbps := data["90th_percentile_download_speed"]["value"],
+        megabits_to_bits(download_mbps),
+        upload_mbps := data["90th_percentile_upload_speed"]["value"],
+        megabits_to_bits(upload_mbps),
+        1,
+    )
 
 
-# Define route to update and return Prometheus metrics
 @app.route("/metrics")
-def updateResults():
+def update_results() -> Callable:
+    """Update Prometheus metrics."""
     global cache_until
 
-    # Run the speed test and update the metrics if the cache has expired
-    if datetime.datetime.now() > cache_until:
-        (
-            r_server_city,
-            r_server_region,
-            r_ping,
-            r_jitter,
-            r_download_mbps,
-            r_download,
-            r_upload_mbps,
-            r_upload,
-            r_status,
-        ) = runTest()
-        server.info(
-            {
-                "server_location_city": str(r_server_city),
-                "server_location_region": str(r_server_region),
-            }
-        )
-        jitter.set(r_jitter)
-        ping.set(r_ping)
-        download_speed.set(r_download)
-        upload_speed.set(r_upload)
-        up.set(r_status)
-        logging.info(
-            "Server City="
-            + str(r_server_city)
-            + " Server Region="
-            + str(r_server_region)
-            + " Jitter="
-            + str(r_jitter)
-            + "ms"
-            + " Ping="
-            + str(r_ping)
-            + "ms"
-            + " Download="
-            + str(r_download_mbps)
-            + "Mbps"
-            + " Upload="
-            + str(r_upload_mbps)
-            + "Mbps"
-        )
+    if datetime.datetime.now() <= cache_until:
+        return make_wsgi_app()
 
-        cache_until = datetime.datetime.now() + datetime.timedelta(
-            seconds=cache_seconds
-        )
+    result = run_test()
+    server.info(
+        {
+            "server_location_city": result.server_city,
+            "server_location_region": result.server_region,
+        }
+    )
+    jitter.set(result.jitter)
+    ping.set(result.ping)
+    download_speed.set(result.download_bps)
+    upload_speed.set(result.upload_bps)
+    up.set(result.status)
+    logging.info(
+        "Server City=%s "
+        "Server Region=%s "
+        "Jitter=%sms "
+        "Ping=%sms "
+        "Download=%sMbit/s "
+        "Upload=%sMbit/s",
+        result.server_city,
+        result.server_region,
+        result.jitter,
+        result.ping,
+        result.download_mbps,
+        result.upload_mbps,
+    )
 
+    cache_until = datetime.datetime.now() + CACHE_SECS
     return make_wsgi_app()
 
 
-# Define route for the main page
 @app.route("/")
-def mainPage():
+def main_page() -> str:
+    """Build the main webpage."""
     return (
         "<h1>Welcome to Cloudflare-Speedtest-Exporter.</h1>"
         + "Click <a href='/metrics'>here</a> to see metrics."
     )
 
 
-# Define function to check for the presence of the cfspeedtest binary
-def checkForBinary():
+def check_cfspeedtest() -> None:
+    """Check for the presence of cfspeedtest and exit if it is not found."""
     if which("cfspeedtest") is None:
         logging.error(
             "Cloudflare-Speedtest CLI binary not found.\n"
-            + "Please install it by running\n"
-            + "'pip install cloudflarepycli'\n"
-            + "https://pypi.org/project/cloudflarepycli/"
+            "Please install it by running 'pip install cloudflarepycli'\n"
+            "https://pypi.org/project/cloudflarepycli/"
         )
-        exit(1)
+        sys.exit(1)
 
 
-# Start the application if this script is run directly
 if __name__ == "__main__":
-    checkForBinary()
-    PORT = os.getenv("SPEEDTEST_PORT", 9798)
+    check_cfspeedtest()
+    PORT = int(os.getenv("SPEEDTEST_PORT", "9798"))
     logging.info(
-        "Starting Cloudflare-Speedtest-Exporter on http://localhost:"
-        + str(PORT)
+        "Starting Cloudflare-Speedtest-Exporter on http://localhost:%s",
+        PORT,
     )
     serve(app, host="0.0.0.0", port=PORT)
